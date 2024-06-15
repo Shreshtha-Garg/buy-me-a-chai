40355a6ab12d081fef33f9a77e433703f1e2b653
Shreshtha Garg <159310162+Shreshtha-Garg@users.noreply.github.com>
2024-06-15 12:24:13 +0530

    Update PaymentPage.js

--- components/PaymentPage.js
+++ components/PaymentPage.js
@@ -420,9 +420,9 @@ const PaymentPage = ({ username }) => {
                                         <button className="bg-slate-700 hover:bg-slate-600 p-2 md:p-3 rounded-lg w-1/4 disabled:bg-slate-500 " disabled={paymentform.name?.length < 3 || paymentform.message?.length < 4 || paymentform.amount < 1} onClick={() => pay(Number.parseInt(paymentform.amount))}>Pay</button>
                                     </div>
                                     <div className="flex flex-row gap-2 2xl:gap-4 mt-5">
-                                        <button className="bg-slate-700 hover:bg-slate-600 2xl:p-4 p-2.5 rounded-lg w-full" onClick={() => pay(5)}>Donate ₹50</button>
-                                        <button className="bg-slate-700 hover:bg-slate-600 2xl:p-4 p-2.5 rounded-lg w-full" onClick={() => pay(10)}>Donate ₹100</button>
-                                        <button className="bg-slate-700 hover:bg-slate-600 2xl:p-4 p-2.5 rounded-lg w-full" onClick={() => pay(20)}>Donate ₹500</button>
+                                        <button className="bg-slate-700 hover:bg-slate-600 2xl:p-4 p-2.5 rounded-lg w-full" onClick={() => pay(50)}>Donate ₹50</button>
+                                        <button className="bg-slate-700 hover:bg-slate-600 2xl:p-4 p-2.5 rounded-lg w-full" onClick={() => pay(100)}>Donate ₹100</button>
+                                        <button className="bg-slate-700 hover:bg-slate-600 2xl:p-4 p-2.5 rounded-lg w-full" onClick={() => pay(500)}>Donate ₹500</button>
                                     </div>
                                 </div>) : (
                                     //show that this user has not set up payment yet
